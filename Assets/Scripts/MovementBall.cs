8bcd694eeeb016d515de8b546f08bfe00011e221
AikaMinami <aikatan.aikachan@gmail.com>
2022-02-15 08:46:05 +0700

    Problem 1, 2, 3

---
+++ Assets/Scripts/MovementBall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBall : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+ 
+
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        rigidBody2D.AddForce(new Vector2(45.0f, 13.0f));
+        
+    }
+}
---
+++ Assets/Scripts/MovementBall_Scnd.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementBall_Scnd : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public float xForce;
+    public float yForce;
+   
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+
+        Debug.Log(4+6/5*2);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            rigidBody2D.velocity = new Vector2(0, yForce);
+        } 
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            rigidBody2D.velocity = new Vector2(0, -yForce);
+        } 
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            rigidBody2D.velocity = new Vector2(xForce, 0);
+        } 
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            rigidBody2D.velocity = new Vector2(-xForce, 0);
+        } 
+    }
+}
